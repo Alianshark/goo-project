983355809a981490877cdfee9f009d4631962af4
Enormouse <johnnysalvia@gmail.com>
2023-04-08 03:59:17 +0300

    add array of ball on click

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let ballNumber = 0;
 let mars = {
     x:100,
     y:300,
@@ -7,19 +8,15 @@ let mars = {
     vx: 0,
     vy: 0,
 }
-
+const balls = [];
 const fps = 1000/60;
 const g = 1;
 let forceY = g; 
 let forceX = 0; 
-
 function time () {
-    mars.img.style.top = mars.y + 'px';
-    mars.img.style.left = mars.x + 'px';
-
-    mars.y += mars.vy;
-    mars.x += mars.vx;
-
+    
+    balls.forEach(drawBall);
+    balls.forEach(moveBall);
     
     if (mars.y > window.innerWidth - mars.heigth/2) {
         mars.vy = -mars.vy;
@@ -31,4 +28,35 @@ function time () {
     
 }
 
+function drawBall (ball) {
+    ball.img.className = 'ball';
+    ball.img.style.top = ball.y + 'px';
+    ball.img.style.left = ball.x + 'px';
+    
+}
+
+function moveBall(ball) {
+    ball.y += ball.vy;
+    ball.x += ball.vx;
+}
+
+function handleMouseClick (event) {
+    const id = 'mars-' + ballNumber;
+    document.body.innerHTML += `<img src="mars.png" id=${id}>` ;
+    const newBall = {
+        x: event.x, 
+        y: event.y, 
+        width:100, 
+        heigth:100, 
+        img: document.querySelector('#'+ id), 
+        vx:0 ,
+        vy:0
+    };
+    
+    balls.push(newBall);
+    ballNumber +=1;
+ 
+}
+
+document.addEventListener('click', handleMouseClick);
 setInterval(time, fps);
